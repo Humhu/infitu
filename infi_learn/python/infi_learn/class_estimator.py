33aa495e80a68e4bb68c4f052501b84ed8325a59
Humphrey Hu <humhu@cmu.edu>
2017-12-06 17:22:41 -0500

    Implemented Parzen hyperparam optimization and added test

--- infi_learn/python/infi_learn/class_estimator.py
+++ infi_learn/python/infi_learn/class_estimator.py
@@ -1,19 +1,29 @@
-"""Classes for using embeddings to predict success
+"""Classes for using X to predict success
 """
-import math
 import numpy as np
+import scipy.stats as ss
 import scipy.spatial.distance as ssd
-import infi_learn as rr
 import sklearn.neighbors as skn
-import scipy.stats as sps
 
 
-def rbf_window(x, xq, bandwidth):
+def rbf_window(x, xq, bw):
     """A vectorized radial basis function kernel
     """
-    dists = ssd.cdist(x, xq, metric='euclidean')
-    return np.exp(-dists / bandwidth)
+    #dists = ssd.cdist(x, xq, metric='euclidean')
+    #return np.exp(-dists / (2 * bw * bw))
+    
+    # return np.atleast_1d(ss.multivariate_normal.pdf(x=x-xq, cov=np.diag(bw)))
+    
+    D = np.diag(bw)
+    deltas = (x - xq).T
+    ips = np.einsum('ji,jk,ki->i',deltas, D, deltas)
+    return np.exp( -ips * 0.5 )
 
+
+def compute_classification_loss(classifier, x, y, params):
+    classifier.set_log_params(params)
+    pp = classifier.query(x)
+    return np.mean(np.abs(y - pp))
 
 def regularized_parzen_estimate(xq, x, y, window, epsilon=0):
     """Returns probability of positive class at query xq
@@ -41,39 +51,59 @@ def regularized_parzen_estimate(xq, x, y, window, epsilon=0):
     K = window(x, xq)
     pos_factor = np.sum(K[y], axis=0) + epsilon
     tot_factor = np.sum(K, axis=0) + 2 * epsilon
-    return pos_factor / tot_factor
+    return np.atleast_1d(pos_factor / tot_factor)
 
 
 class ParzenNeighborsClassifier(object):
     """A binary classifier using Parzen windows on nearest-neighbors.
     """
 
-    def __init__(self, radius, epsilon, window=None):
-        self.nn_model = skn.NearestNeighbors(radius=radius)
-        if window is None:
-            self.window = lambda x, xq: rbf_window(x, xq, 1.0)
-        else:
-            self.window = window
+    def __init__(self, bandwidth, epsilon, radius=None, window=None):
+        self.nn_model = skn.NearestNeighbors()
+        self.bw = bandwidth
         self.epsilon = epsilon
 
-        self.embeddings = None
+        if window is None:
+            self.window = rbf_window
+        else:
+            self.window = window
+        if radius is None:
+            # Rule of thumb
+            self.radius = 3 * self.bw
+        else:
+            self.radius = radius
+
+        self.X = None
         self.labels = None
 
-    def update_model(self, embeddings, labels):
+    def set_log_params(self, p):
+        """Set the bandwidth and regularization log-parameters.
+        Assumes [eps, rad, bw] ordering for p.
+        """
+        params = np.exp(p)
+        self.epsilon = params[0]
+        self.radius = params[1]
+        self.bw = params[2:]
+
+    def get_log_params(self):
+        p = np.hstack([self.epsilon, self.radius, self.bw])
+        return np.log(p)
+
+    def update_model(self, X, labels):
         """Regenerates the dataset embedding and retrains the
         nearest neighbor model
 
         Parameters
         ----------
-        embeddings : array of vectors, or numpy 2D
+        X : array of vectors, or numpy 2D
         """
-        self.embeddings = embeddings
+        self.X = X
         self.labels = labels
-        self.nn_model.fit(X=embeddings)
+        self.nn_model.fit(X=X)
 
     def query(self, x):
-        """Predicts the positive class probability and certainty
-        at the specified embedding query points
+        """Predicts the positive class probability at the specified
+        embedding query points
 
         Parameters
         ----------
@@ -82,8 +112,6 @@ class ParzenNeighborsClassifier(object):
 
         Returns
         -------
-        pos : 1D array of bool
-            Predicted class
         probs : 1D array
             Positive class probabilities
         """
@@ -91,18 +119,22 @@ class ParzenNeighborsClassifier(object):
         # NOTE there's a slight speedup here if we use L2 distances for RBF
         # kernel
         inds = self.nn_model.radius_neighbors(x,
+                                              radius=self.radius,
                                               return_distance=False)
 
+        def win_func(x, xq): return self.window(x=x, xq=xq, bw=self.bw)
         probs = []
-        for i in inds:
-            neighbors_x = self.embeddings[i]
+        for j, i in enumerate(inds):
+            if len(i) == 0:
+                probs.append(np.atleast_1d(0.5))
+                continue
+            neighbors_x = self.X[i]
             neighbors_y = self.labels[i]
-            probi = regularized_parzen_estimate(xq=x,
+            probi = regularized_parzen_estimate(xq=x[j],
                                                 x=neighbors_x,
                                                 y=neighbors_y,
-                                                window=self.window,
+                                                window=win_func,
                                                 epsilon=self.epsilon)
             probs.append(probi)
-        probs = np.array(probs)
-        classes = probs >= 0.5 # NOTE rounding 0.5 to positive
-        return classes, probs
+        probs = np.squeeze(probs)
+        return probs
---
+++ infi_learn/test/test_parzen.py
@@ -0,0 +1,120 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import infi_learn as rr
+import optim
+
+# 1. Generate data
+n_dim = 2
+n_clusters = 1
+
+
+def sample_points(n_train, n_test, offset):
+    centers = np.random.uniform(low=-1, high=1, size=(n_clusters, n_dim))
+    covs = []
+    for _ in range(n_clusters):
+        covs.append(np.diag(np.random.uniform(size=n_dim)))
+
+    def sample(n):
+        return [np.random.multivariate_normal(x, C, size=n)
+                for x, C in zip(centers, covs)]
+    train_points = np.vstack(sample(n_train)) + offset
+    test_points = np.vstack(sample(n_test)) + offset
+    return train_points, test_points
+
+
+n_train_pts_cluster = 1000
+n_test_pts_cluster = 100
+
+pos_offset = np.random.uniform(low=-1, high=1, size=n_dim)
+pos_train_pts, pos_test_pts = sample_points(n_train_pts_cluster,
+                                            n_test_pts_cluster,
+                                            pos_offset)
+neg_offset = np.random.uniform(low=-1, high=1, size=n_dim)
+neg_train_pts, neg_test_pts = sample_points(n_train_pts_cluster,
+                                            n_test_pts_cluster,
+                                            neg_offset)
+pos_train_labels = np.ones(len(pos_train_pts), dtype=bool)
+pos_test_labels = np.ones(len(pos_test_pts), dtype=bool)
+neg_train_labels = np.zeros(len(neg_train_pts), dtype=bool)
+neg_test_labels = np.zeros(len(neg_test_pts), dtype=bool)
+
+train_pts = np.vstack((pos_train_pts, neg_train_pts))
+train_labels = np.hstack((pos_train_labels, neg_train_labels))
+test_pts = np.vstack((pos_test_pts, neg_test_pts))
+test_labels = np.hstack((pos_test_labels, neg_test_labels))
+
+plt.ion()
+
+# 2. Train models
+classifier = rr.ParzenNeighborsClassifier(bandwidth=[0.5, 0.5],
+                                          epsilon=1e-6,
+                                          radius=1)
+
+
+classifier.update_model(X=train_pts, labels=train_labels)
+
+mins = np.min(np.vstack((pos_train_pts, neg_train_pts)), axis=0)
+maxs = np.max(np.vstack((pos_train_pts, neg_train_pts)), axis=0)
+n_vis = 30
+vis_lims = [np.linspace(start=l, stop=u, num=n_vis)
+            for l, u in zip(mins, maxs)]
+vis_pts = np.meshgrid(*vis_lims)
+vis_x = np.vstack([vi.flatten() for vi in vis_pts]).T
+
+
+def reshape_vis(x):
+    return np.reshape(x, (n_vis, n_vis))
+
+
+def visualize(title, vis_train=False):
+    pos_vis_prob = classifier.query(vis_x)
+
+    plt.figure()
+    #plt.contour(vis_pts[0], vis_pts[1], reshape_vis(pos_vis_prob))
+    plt.imshow(reshape_vis(pos_vis_prob), origin='lower',
+               extent=(mins[0], maxs[0], mins[1], maxs[1]),
+               cmap=cm.bwr,
+               vmin=0, vmax=1)
+    if vis_train:
+        plt.plot(pos_train_pts[:, 0], pos_train_pts[:, 1], 'ko', mew=0.01)
+        plt.plot(neg_train_pts[:, 0], neg_train_pts[:, 1], 'kx', mew=1.0)
+    else:
+        plt.plot(pos_test_pts[:, 0], pos_test_pts[:, 1], 'ko', mew=0.01)
+        plt.plot(neg_test_pts[:, 0], neg_test_pts[:, 1], 'kx', mew=1.0)
+    plt.title(title)
+    plt.colorbar()
+
+
+def obj(p):
+    return rr.compute_classification_loss(classifier=classifier,
+                                          x=test_pts,
+                                          y=test_labels,
+                                          params=p)
+
+
+def print_params():
+    print 'bandwidth: %s epsilon: %f radius: %f' % \
+        (str(classifier.bw), classifier.epsilon, classifier.radius)
+
+
+# 3. Perform hyperparameter optimization
+p0 = classifier.get_log_params()
+visualize('Initial validation loss is %f' % obj(p0))
+print_params()
+
+cma_optimizer = optim.CMAOptimizer(mode='min')
+cma_optimizer.lower_bounds = [-15, -2, -2, -2]
+cma_optimizer.upper_bounds = [0, 2, 2, 2]
+
+pcma = cma_optimizer.optimize(x_init=p0, func=obj)[0]
+visualize('CMA final validation loss is %f' % obj(pcma))
+print_params()
+
+bfgs_optimizer = optim.BFGSOptimizer(mode='min', num_restarts=3)
+bfgs_optimizer.lower_bounds = [-15, -2, -2, -2]
+bfgs_optimizer.upper_bounds = [0, 2, 2, 2]
+
+pbfgs = bfgs_optimizer.optimize(x_init=p0, func=obj)[0]
+visualize('BFGS final validation loss is %f' % obj(pbfgs))
+print_params()
